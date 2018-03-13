ece4ef9e39253c926b1e5aafeed16bfe5dce1158
Ricardo <legoricardo@hotmail.com>
2018-03-13 00:46:34 -0600

    Revert ":V"
    This reverts commit 274dddb7c1741e138cd9f6f754c32ec693c5dc5a.

--- Parcial.X/build/default/production/config.p1.d
+++
@@ -1,5 +0,0 @@
- build/default/production/config.d  \
- build/default/production/config.p1:  \
- config.c  \
-config.h  \
-globalDefinitions.h 
--- Parcial.X/build/default/production/funciones.p1.d
+++
@@ -1,7 +0,0 @@
- build/default/production/funciones.d  \
- build/default/production/funciones.p1:  \
- funciones.c  \
-lcd.h  \
-config.h  \
-funciones.h  \
-globalDefinitions.h 
