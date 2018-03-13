68653029dfff4aa69ad45515c9956d31a388e90c
Ricardo <legoricardo@hotmail.com>
2018-03-13 00:46:39 -0600

    Revert "Revert ":V""
    This reverts commit ece4ef9e39253c926b1e5aafeed16bfe5dce1158.

---
+++ Parcial.X/VariableGlobal.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   VariableGlobal.h
+ * Author: Administrador
+ *
+ * Created on 2 de marzo de 2018, 03:29 PM
+ */
+
+#ifndef VARIABLEGLOBAL_H
+#define	VARIABLEGLOBAL_H
+
+    #include <xc.h>
+
+
+#endif	/* VARIABLEGLOBAL_H */
+
